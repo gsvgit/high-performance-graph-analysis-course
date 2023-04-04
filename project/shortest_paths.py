701c1652cc62adc91d52d7a70a5edb36161964c3
esvault <azarnikov01@gmail.com>
2023-04-04 13:40:08 +0300

    [Task 4]

---
+++ project/shortest_paths.py
@@ -0,0 +1,88 @@
+from pygraphblas import Matrix, FP64
+import math
+
+from typing import List, Tuple
+
+
+def sssp(graph: Matrix, source: int) -> List[int]:
+    """
+    Search the shortest paths for single source
+    :param graph: float adjacency matrix of graph
+    :param source: source vertex
+    :return: list where i-th element is distance from source to vertex i
+    """
+    return mssp(graph, [source])[0][1]
+
+
+def mssp(graph: Matrix, sources: List[int]) -> List[Tuple[int, List[int]]]:
+    """
+    Search the shortest paths for multiple sources
+    :param graph: float adjacency matrix of graph
+    :param sources: list of source vertices
+    :return: list where i-th element is the pair (source, distances) for i-th vertex
+    """
+
+    assert graph.type == FP64
+    assert graph.square
+    for source in sources:
+        assert source in range(graph.nrows)
+
+    graph = _zeros_on_main_diag(graph)
+
+    dists = Matrix.sparse(FP64, nrows=len(sources), ncols=graph.nrows)
+    for i, source in enumerate(sources):
+        dists[i, source] = 0
+
+    for _ in range(graph.nrows - 1):
+        dists = dists.mxm(graph, FP64.MIN_PLUS)
+
+    if dists.isne(dists.mxm(graph, FP64.MIN_PLUS)):
+        raise ValueError("Negative cycle detected")
+
+    return [
+        (source, [dists.get(i, j, default=math.inf) for j in range(graph.nrows)])
+        for i, source in enumerate(sources)
+    ]
+
+
+def apsp(graph: Matrix) -> List[Tuple[int, List[int]]]:
+    """
+    Search the shortest paths for all pair of vertices
+    :param graph: float adjacency matrix of graph
+    :return: list where i-th element is pair (source, dist) for i-th vertex
+    """
+
+    assert graph.type == FP64
+    assert graph.square
+
+    graph = _zeros_on_main_diag(graph)
+    dists = graph
+
+    for i in range(graph.nrows):
+        col, row = dists.extract_matrix(col_index=i), dists.extract_matrix(row_index=i)
+        dists.eadd(
+            col.mxm(row, FP64.MIN_PLUS),
+            FP64.MIN,
+            out=dists,
+        )
+
+    for k in range(graph.nrows):
+        col, row = dists.extract_matrix(col_index=k), dists.extract_matrix(row_index=k)
+        if dists.isne(
+            dists.eadd(
+                col.mxm(row, FP64.MIN_PLUS),
+                FP64.MIN,
+            )
+        ):
+            raise ValueError("Negative cycle detected")
+
+    return [
+        (i, [dists.get(i, j, default=math.inf) for j in range(graph.nrows)]) for i in range(graph.nrows)
+    ]
+
+
+def _zeros_on_main_diag(matrix: Matrix) -> Matrix:
+    copy = matrix.dup()
+    for i in range(matrix.ncols):
+        copy[i, i] = 0.0
+    return copy
---
+++ tests/paths_test.py
@@ -0,0 +1,74 @@
+import project.shortest_paths as sp
+from pygraphblas import Matrix
+from math import inf
+
+# test_data_ms_bfs = (
+#     Matrix.from_lists([0, 1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 5], True),
+#     [
+#         ([1], [(1, [-2, -1, 1, 2, 3, 4])]),
+#         ([1, 2], [(1, [-2, -1, 1, 2, 3, 4]), (2, [-2, -2, -1, 2, 3, 4])]),
+#         (
+#             [0, 1, 2, 3, 4],
+#             [
+#                 (0, [-1, 0, 1, 2, 3, 4]),
+#                 (1, [-2, -1, 1, 2, 3, 4]),
+#                 (2, [-2, -2, -1, 2, 3, 4]),
+#                 (3, [-2, -2, -2, -1, 3, 4]),
+#                 (4, [-2, -2, -2, -2, -1, 4]),
+#             ],
+#         ),
+#     ],
+# )
+
+test_data_sssp = {
+    'graph': Matrix.from_lists([0, 1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 5], [1.0, 2.0, 3.0, 3.0, 2.0, 1.0]),
+    'sources': [0, 1, 2, 3, 4, 5],
+    'ans': [
+        [0.0, 1.0, 3.0, 6.0, 9.0, 11.0],
+        [inf, 0.0, 2.0, 5.0, 8.0, 10.0],
+        [inf, inf, 0.0, 3.0, 6.0, 8.0],
+        [inf, inf, inf, 0.0, 3.0, 5.0],
+        [inf, inf, inf, inf, 0.0, 2.0],
+        [inf, inf, inf, inf, inf, 0.0]
+    ]
+}
+
+test_data_mssp = {
+    'graph': Matrix.from_lists([0, 1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 5], [1.0, 2.0, 3.0, 3.0, 2.0, 1.0]),
+    'sources': [
+        [0, 1],
+        [2, 3],
+        [0, 5]
+    ],
+    'ans': [
+        [(0, [0.0, 1.0, 3.0, 6.0, 9.0, 11.0]), (1, [inf, 0.0, 2.0, 5.0, 8.0, 10.0])],
+        [(2, [inf, inf, 0.0, 3.0, 6.0, 8.0]), (3, [inf, inf, inf, 0.0, 3.0, 5.0])],
+        [(0, [0.0, 1.0, 3.0, 6.0, 9.0, 11.0]), (5, [inf, inf, inf, inf, inf, 0.0])]
+    ]
+}
+
+test_data_apsp = {
+    'graph': Matrix.from_lists([0, 1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 5], [1.0, 2.0, 3.0, 3.0, 2.0, 1.0]),
+    'ans': [
+        (0, [0.0, 1.0, 3.0, 6.0, 9.0, 11.0]),
+        (1, [inf, 0.0, 2.0, 5.0, 8.0, 10.0]),
+        (2, [inf, inf, 0.0, 3.0, 6.0, 8.0]),
+        (3, [inf, inf, inf, 0.0, 3.0, 5.0]),
+        (4, [inf, inf, inf, inf, 0.0, 2.0]),
+        (5, [inf, inf, inf, inf, inf, 0.0])
+    ]
+}
+
+
+def test_sssp():
+    for i, s in enumerate(test_data_sssp['sources']):
+        assert sp.sssp(test_data_sssp['graph'], s) == test_data_sssp['ans'][i]
+
+
+def test_mssp():
+    for i, s in enumerate(test_data_mssp['sources']):
+        assert sp.mssp(test_data_mssp['graph'], s) == test_data_mssp['ans'][i]
+
+
+def test_apsp():
+    assert sp.apsp(test_data_apsp['graph']) == test_data_apsp['ans']
